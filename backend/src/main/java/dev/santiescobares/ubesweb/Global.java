59a3441c87aaccee3e76a260ff288502fb25b026
Santiago Escobares <santiagoescobares06@gmail.com>
2026-04-16 21:19:43 -0300

    feat: create global variables class

---
+++ backend/src/main/java/dev/santiescobares/ubesweb/Global.java
@@ -0,0 +1,17 @@
+package dev.santiescobares.ubesweb;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class Global {
+
+    public final String BASE_URL = "/api/v1";
+    public final String PROD_URL = "http://localhost:5173";
+
+    public final String ACCESS_TOKEN_COOKIE = "access_token";
+
+    public final String R2_PUBLIC_URL = "https://pub-6e3ddb68c9054b32afce9c1d8ae47d40.r2.dev";
+
+    public final String REDIS_TOKEN_BLACKLIST_KEY = "token_blacklist:";
+    public final String REDIS_FORCED_LOGOUT_KEY = "forced_logout:";
+}
