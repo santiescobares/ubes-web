f53b617540ab1cb08c75f24e7c330861b1397d3b
Santiago Escobares <santiagoescobares06@gmail.com>
2026-04-17 17:32:32 -0300

    feat: create storage service

--- backend/src/main/java/dev/santiescobares/ubesweb/Global.java
+++ backend/src/main/java/dev/santiescobares/ubesweb/Global.java
@@ -11,6 +11,7 @@ public final class Global {
     public final String ACCESS_TOKEN_COOKIE = "access_token";
 
     public final String R2_PUBLIC_URL = "https://pub-6e3ddb68c9054b32afce9c1d8ae47d40.r2.dev";
+    public final String R2_USER_PICTURES_PATH = "user-pictures/";
 
     public final String REDIS_TOKEN_BLACKLIST_KEY = "token_blacklist:";
     public final String REDIS_FORCED_LOGOUT_KEY = "forced_logout:";
